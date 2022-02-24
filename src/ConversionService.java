1db201423e542b7e2d78a34549e6417b0aae99c3
Isaac Smith <is.smith.962@gmail.com>
2022-02-24 00:15:37 -0700

    Initial Commit + method stub for conversion

---
+++ src/ConversionService.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.Soundbank;
+
+public class ConversionService {
+    public ConversionService() {}
+
+    public float convertCurrency(String currencyFrom, String currencyTo, float amount) {
+        return 0; 
+    }
+}
+    
