d0172fa58cde4692dc4e79fb5f9c2df7492eb20e
Isaac Smith <is.smith.962@gmail.com>
2022-02-23 23:38:16 -0700

    Created Currency object for containing (currency code, exchange rate) pair

---
+++ src/Currency.java
@@ -0,0 +1,17 @@
+public class Currency {
+    private String currencyCode; 
+    private double exchangeRate; 
+
+    public Currency(String currencyCode, double exchangeRate) {
+        this.currencyCode = currencyCode; 
+        this.exchangeRate = exchangeRate; 
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode; 
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate; 
+    }
+}
