433dcec9a166bc4c8d596844831f45924a088fc8
Isaac Smith <is.smith.962@gmail.com>
2022-02-24 00:16:05 -0700

    Removed stub, in wrong class

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,9 +1,5 @@
 public class CurrencyConverter {
     public static void main(String[] args) {
-
-    }
-
-    public float convertCurrency(String currencyFrom, String currencyTo, float amount) {
-        return 0; 
+        
     }
 }
