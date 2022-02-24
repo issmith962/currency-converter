e9ac9782f575a82470e113d5dbbf2daeb83ac2c6
Isaac Smith <is.smith.962@gmail.com>
2022-02-24 00:10:58 -0700

    Added converter method stub

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,5 +1,9 @@
 public class CurrencyConverter {
     public static void main(String[] args) {
-        
+
+    }
+
+    public float convertCurrency(String currencyFrom, String currencyTo, float amount) {
+        return 0; 
     }
 }
