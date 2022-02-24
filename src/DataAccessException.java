62f0ce94cef7a78a6619217036890b091fe1c19d
Isaac Smith <is.smith.962@gmail.com>
2022-02-23 23:39:34 -0700

    Created custom DAO exception

---
+++ src/DataAccessException.java
@@ -0,0 +1,11 @@
+public class DataAccessException extends Exception {
+    private String message; 
+    
+    public DataAccessException(String message) {
+        this.message = message; 
+    }
+
+    public String getMessage() {
+        return message; 
+    }
+}
